c6b95f9df01d59f5e229bf361315d758cc008917
Eylam Milner <Eylam Milner@DESKTOP-3RLPP99>
2016-10-08 15:01:49 +0300

    Added Review M + V + C

---
+++ MovieNight/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MovieNight.Models
+{
+    public class Review
+    {
+        public int ID { get; set; }
+        public string CriticName { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public string MovieTitle { get; set; }
+        public string ReviewContent { get; set; }
+    }
+    public class ReviewDBContext : DbContext
+    {
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
