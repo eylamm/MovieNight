74340e70bcbfe807171d95dad08dad95947c0d87
Eylam Milner <Eylam Milner@DESKTOP-3RLPP99>
2016-10-08 15:18:47 +0300

    Adding Director  M + V + C

---
+++ MovieNight/Models/Director.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MovieNight.Models
+{
+    public class Director
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Origin { get; set; }
+    }
+    public class DirectorDBContext : DbContext
+    {
+        public DbSet<Director> Directors { get; set; }
+    }
+}
